d7d73a35016101052b275f9c900cb65623e4ffab
Peeyush-VI <111489991+Peeyush-VI@users.noreply.github.com>
2026-07-30 15:43:49 +0530

    Fix ReactNode version mismatch by casting to any

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -4,9 +4,8 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    // @ts-ignore - ReactLenis types are missing children
     <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
-      {children}
+      {children as any}
     </ReactLenis>
   );
 }
